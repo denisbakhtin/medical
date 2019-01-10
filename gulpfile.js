34927252e9db10f58c795f557a7c889a88f535c0
Denis Bakhtin <denis.bakhtin@gmail.com>
2019-01-10 12:59:11 +0400

    downgrade to gulp 3.9.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-var gulp         = require("gulp"),
-    sass         = require("gulp-sass"),
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
     autoprefixer = require("gulp-autoprefixer"),
-    notify       = require("gulp-notify"),
-    concat       = require("gulp-concat"),
-    gzip         = require("gulp-gzip"),
-    del          = require("del")
+    notify = require("gulp-notify"),
+    concat = require("gulp-concat"),
+    gzip = require("gulp-gzip"),
+    del = require("del")
 
 
 // Compile SCSS files to CSS
@@ -15,15 +15,15 @@ gulp.task("scss", function () {
     //compile hashed css files
     gulp.src("src/scss/**/*.scss")
         .pipe(sass({
-            outputStyle : "compressed",
-            includePaths : [
-              "src/scss",
+            outputStyle: "compressed",
+            includePaths: [
+                "src/scss",
             ]
-        }).on("error", notify.onError(function(error){
-          return "Error: " + error.message;
+        }).on("error", notify.onError(function (error) {
+            return "Error: " + error.message;
         })))
         .pipe(autoprefixer({
-            browsers : ["last 20 versions"]
+            browsers: ["last 20 versions"]
         }))
         .pipe(gulp.dest("public/css"))
 })
@@ -41,36 +41,36 @@ gulp.task("js", function () {
     gulp.src("src/js/ckeditor/**/*")
         .pipe(gulp.dest("public/js/ckeditor"))
     gulp.src([
-      "src/js/jquery-2.1.4.min.js",
-      "src/js/jquery.actual.min.js",
-      "src/js/parsley.min.js",
-      "src/js/bootstrap.min.js",
-      "src/js/jquery.slimscroll.min.js",
-      "src/js/jquery.maskedinput.min.js",
-      "src/js/select2.min.js",
-      "src/js/nouislider.min.js",
-      "src/js/lightbox.min.js",
-      "src/js/application.js",
-    ])
+            "src/js/jquery-2.1.4.min.js",
+            "src/js/jquery.actual.min.js",
+            "src/js/parsley.min.js",
+            "src/js/bootstrap.min.js",
+            "src/js/jquery.slimscroll.min.js",
+            "src/js/jquery.maskedinput.min.js",
+            "src/js/select2.min.js",
+            "src/js/nouislider.min.js",
+            "src/js/lightbox.min.js",
+            "src/js/application.js",
+        ])
         .pipe(concat("application.js"))
         .pipe(gulp.dest("public/js"))
 })
 
 // fonts
-gulp.task('icons', function() {
+gulp.task('icons', function () {
     del(["public/fonts/**/*"])
     gulp.src('src/fonts/**.*')
         .pipe(gulp.dest('public/fonts'));
 });
 
 // gzip
-gulp.task('gzip', function() {
-  gulp.src('public/js/*.js')
-  .pipe(gzip())
-  .pipe(gulp.dest('public/js'));
-  gulp.src('public/css/*.css')
-  .pipe(gzip())
-  .pipe(gulp.dest('public/css'));
+gulp.task('gzip', function () {
+    gulp.src('public/js/*.js')
+        .pipe(gzip())
+        .pipe(gulp.dest('public/js'));
+    gulp.src('public/css/*.css')
+        .pipe(gzip())
+        .pipe(gulp.dest('public/css'));
 });
 
 // Watch asset folder for changes
