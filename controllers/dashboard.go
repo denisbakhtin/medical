ca4c6e0c7a1660058c66023db99ba4bd9c857d67
Denis Bakhtin <denis.bakhtin@gmail.com>
2025-04-18 01:36:15 +0300

    Redirect dashboard home

--- controllers/dashboard.go
+++ controllers/dashboard.go
@@ -4,8 +4,11 @@ import "github.com/gin-gonic/gin"
 
 // Dashboard handles GET /admin route
 func (ap *Application) Dashboard(c *gin.Context) {
-	c.HTML(200, "dashboard/show", gin.H{
-		"Title":         "Панель управления",
-		"Authenticated": true,
-	})
+
+	// c.HTML(200, "dashboard/show", gin.H{
+	// 	"Title":         "Панель управления",
+	// 	"Authenticated": true,
+	// })
+	// no point in showing blank page, just redirect somewhere
+	c.Redirect(302, "/admin/articles")
 }
